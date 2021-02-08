961c240d996480a240095477a4c31fbe5bd522c8
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2021-02-08 11:58:26 -0300

    Working pre-alpha version

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import { config } from 'dotenv';
+import path from 'path';
+
+/**
+    * Configurations
+    * It will initiate the app ( express )
+    * Call the config function for the dotenv file ( .env )
+    * Create the port variable
+**/
+const app = express();
+config();
+const PORT = process.env.PORT;
+
+/**
+    * Custom express configuration
+    * The views part were commented since I'm still deciding whether or not it's worth to have a separated frontend.
+        * For now it will remain commented. 
+**/
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+/*
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+*/
+
+// Routes
+import login from './routes/login';
+import index from './routes/index';
+
+app.use('/', index);
+app.use('/login', login);
+
+app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
