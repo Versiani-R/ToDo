606b47bc3e8e65d9e86eb65aeb1e20830eae694b
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2021-03-02 14:50:09 -0300

    The first deploy of the website was a complete success ( nightmare )

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { config } from 'dotenv';
 
 /**
@@ -12,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 /* Custom express configuration */
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
